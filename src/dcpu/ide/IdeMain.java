4e5e1a7883c3c970834ef1137c229504ddae5a3a
Anton Belokrylov <aimozg@ya.ru>
2012-04-08 13:44:37 +0700

    started working on IDE

---
+++ src/dcpu/ide/IdeMain.java
@@ -0,0 +1,28 @@
+package dcpu.ide;
+
+import javax.swing.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: aimozg
+ * Date: 08.04.12
+ * Time: 12:45
+ */
+public class IdeMain {
+    private JTextArea sourceTextarea;
+    private JTable registersTable;
+    private JTable memoryTable;
+    private JTextArea consoleTextarea;
+    private JButton openSrcButton;
+    private JButton saveSrcButton;
+    private JButton asmButton;
+    private JButton execButton;
+    private JButton runButton;
+    private JButton resetButton;
+    private JButton stepButton;
+    private JButton breakpointButton;
+    private JButton saveBinButton;
+    private JButton openBinButton;
+    private JButton pauseButton;
+    private JButton clearButton;
+}
