9efb57a91782ec83057a31d274ca7151541780e1
Mark Fisher <mark.j.fisher@gmail.com>
2012-04-24 00:59:45 +0100

    [MF] Added HighNerd demo - runs Notches sample dump

---
+++ src/dcpu/demos/HighNerd.java
@@ -0,0 +1,49 @@
+package dcpu.demos;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ShortBuffer;
+
+import computer.AWTKeyMapping;
+import computer.VirtualKeyboard;
+import computer.VirtualMonitor;
+
+import dcpu.Dcpu;
+import dcpu.io.PanelPeripheral;
+
+public class HighNerd {
+
+    private Dcpu dcpu;
+
+    public static void main(String[] args) throws IOException {
+        new HighNerd().startDemo();
+    }
+
+    private void startDemo() throws IOException {
+        dcpu = new Dcpu();
+        VirtualMonitor display = new VirtualMonitor(dcpu.mem, 0x8000);
+        VirtualKeyboard keyboard = new VirtualKeyboard(dcpu.mem, 0x9000, new AWTKeyMapping());
+        PanelPeripheral panelPeripheral = new PanelPeripheral(display, keyboard);
+        dcpu.attach(panelPeripheral, -1);
+        dcpu.upload(getShortsFromResource("mem.dmp"));
+        dcpu.reset();
+        dcpu.run();
+    }
+
+    private short[] getShortsFromResource(String resourceName) throws IOException {
+        int nRead;
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream(2048);
+        byte[] data = new byte[0x4000];
+        while ((nRead = HighNerd.class.getResourceAsStream(resourceName).read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, nRead);
+        }
+        buffer.flush();
+        ByteBuffer bb = ByteBuffer.wrap(buffer.toByteArray());
+        ShortBuffer sb = bb.asShortBuffer();
+        short[] shorts = new short[sb.limit()];
+        sb.get(shorts);
+        return shorts;
+    }
+
+}
